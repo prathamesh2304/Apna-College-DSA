c1779686534136c4bed04702a6b5c37a485494f2
prathamesh2304 <prathameshthakur.engg@gmail.com>
2024-10-16 02:04:27 +0530

    Function Overloading With Differnet Para

--- Day 5/D5FunOverLoad.java
+++ Day 5/D5FunOverLoad.java
@@ -5,8 +5,13 @@ public class D5FunOverLoad {
     public static int Multiply(int a, int b, int c){
         return a*b*c;
     }
+    public static int Multiply(int a,int b,int c, int d){
+        return a*b*c*d;
+    }
+    
     public static void main(String[] args) {
         System.out.println(Multiply(12, 32));
         System.out.println(Multiply(6, 32,2));
+        System.out.println(Multiply(6, 6,2,6));
     }
 }
