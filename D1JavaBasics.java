47979881a454b05949a8575d414637dd3ec7da90
prathamesh2304 <prathameshthakur.engg@gmail.com>
2024-10-15 20:11:58 +0530

    Day Wise Sorted

--- D1JavaBasics.java
+++
@@ -1,9 +0,0 @@
-public class D1JavaBasics {
-    public D1JavaBasics() {
-    }
- 
-    public static void main(String[] var0) {
-       System.out.println("Hello");
-    }
- }
- 
--- Practice/HW1.java
+++
@@ -1,7 +0,0 @@
-package Practice;
-
-public class HW1 {
-    public static void main(String[] args) {
-        System.out.println("Hello Ayya ");
-    }
-}
