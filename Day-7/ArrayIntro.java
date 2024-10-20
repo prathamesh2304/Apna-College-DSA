1b9469e99e7387cd5152badbf9b195594032857a
prathamesh2304 <prathameshthakur.engg@gmail.com>
2024-10-21 02:00:17 +0530

    Array Introduction

---
+++ Day-7/ArrayIntro.java
@@ -0,0 +1,15 @@
+public class ArrayIntro {
+    public static void main(String[] args) {
+        int subMaks[] = new int [50]; // Array Declaration 
+        int classTest[] = {10,8,9};
+        String StudentName[] = {"Prathamesh","Aniket","Shivam"};
+
+        subMaks[0] = 98;
+        subMaks[1] = 90;
+        subMaks[2] = 99;
+
+        System.out.println("Subject 1 ="+subMaks[0]+ " Subject 2 ="+subMaks[1]+ " Subject 3 ="+subMaks[2]);
+        
+
+    }
+}
